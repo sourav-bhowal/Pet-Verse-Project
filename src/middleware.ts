641fa88cec52983abb9bcb32b995e12c6d56f90f
Sourav Bhowal <souravbhowal15@gmail.com>
2024-06-16 15:54:27 +0530

    fix9

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,9 @@ import { getToken } from "next-auth/jwt";
 export { default } from "next-auth/middleware";
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
+    const token = await getToken({req: request });
     const url = request.nextUrl;
-    console.log(token);
+    console.log(url.pathname);
 
     // Redirect to dashboard if the user is already authenticated
     // and trying to access sign-in, sign-up, or home page
