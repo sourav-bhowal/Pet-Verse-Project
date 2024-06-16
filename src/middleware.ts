d004686d8945ef6a6536d1dbeac9ca6db0bdfb90
Sourav Bhowal <souravbhowal15@gmail.com>
2024-06-16 15:36:45 +0530

    r

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export { default } from "next-auth/middleware";
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request });
     const url = request.nextUrl;
+    console.log(token);
 
     // Redirect to dashboard if the user is already authenticated
     // and trying to access sign-in, sign-up, or home page
